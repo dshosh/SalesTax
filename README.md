e80fda956439631a6bcb00e424cd622773a467e9
Dmitri Shosh <dmitri_shosh@dialog.com.au>
2020-11-30 07:12:10 +1100

    Add project files.

---
+++ README.md
@@ -0,0 +1,7 @@
+Sales Tax Problem
+-----------------
+
+* Basic sales tax is applicable at a rate of 10% on all goods, except books, food, and medical products that are exempt. 
+* Import duty is an additional sales tax applicable on all imported goods at a rate of 5%, with no exemptions.
+* Tax is calculated per product and is rounded to the nearest 5 cents.
+* Invoice must lists the name of all the products and their price (including tax), finishing with the total cost of the items, and the total amounts of sales taxes paid.
---
+++ SalesTax.Tests/AllTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using SalesTax.Lib;
+using Xunit;
+
+namespace SalesTax.Tests
+{
+    public class AllTests
+    {
+        // Use Banker Rounding strategy:
+        readonly ITaxCalculator _calculator = new TaxCalculator(d => Math.Round(d * 20) / 20M);
+
+        [Fact]
+        public void Produces_a_valid_receipt_for_a_single_line_sale()
+        {
+            var sale = new SalesOrder(_calculator);
+            sale.Add(OrderItemFactory.CreateItem("1 book at 12.49"));
+            var expected = "1 book: $12.49\nSales Taxes: $0.00\nTotal: $12.49";
+            var actual = sale.ToString();
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Produces_a_valid_receipt_for_a_multi_line_sale()
+        {
+            var sale = new SalesOrder(_calculator);
+            sale.Add(OrderItemFactory.CreateItem("10 imported bottles of whiskey at 27.99"));
+            sale.Add(OrderItemFactory.CreateItem("10 bottles of local whiskey at 18.99"));
+            sale.Add(OrderItemFactory.CreateItem("10 packets of iodine tablets at 9.75"));
+            sale.Add(OrderItemFactory.CreateItem("10 boxes of imported potato chips at 11.25"));
+            var expected = "10 imported bottles of whiskey: $321.90\n10 bottles of local whiskey: $208.90\n10 packets of iodine tablets: $97.50\n10 imported boxes of potato chips: $118.10\nSales Taxes: $66.60\nTotal: $746.40";
+            var actual = sale.ToString();
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Correct_sale_when_new_product_added_to_registry()
+        {
+            TaxFreeRegistry.Add("tomato");
+            var sale = new SalesOrder(_calculator);
+            sale.Add(OrderItemFactory.CreateItem("2 tomatoes at 20"));
+            var expected = "2 tomatoes: $40.00\nSales Taxes: $0.00\nTotal: $40.00";
+            var actual = sale.ToString();
+            Assert.Equal(expected, actual);
+        }
+    }
+}
